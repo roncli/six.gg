8d9d8064a107985229a23e3b490f01063c579bda
Ronald M. Clifford <roncli@roncli.com>
2021-10-11 23:34:59 -0700

    Fix MongoDb aggregate function typing.

--- node/types/node/db.d.ts
+++ node/types/node/db.d.ts
@@ -4,6 +4,7 @@ import { EventMongoData } from "./eventTypes"
 import { EncryptedMongoSessionData } from "./sessionTypes"
 import { EncryptedMongoTokens } from "./twitchTypes"
 import { UserMongoData } from "./userTypes"
+import MongoDb from "mongodb"
 
 declare module "mongodb" {
     export interface Db {
@@ -13,5 +14,10 @@ declare module "mongodb" {
         collection<TSchema = EncryptedMongoSessionData>(name: "session", options?: CollectionOptions): Collection<TSchema>
         collection<TSchema = EncryptedMongoTokens>(name: "twitch", options?: CollectionOptions): Collection<TSchema>
         collection<TSchema = UserMongoData>(name: "user", options?: CollectionOptions): Collection<TSchema>
+    }
+
+    // The default implementation of aggregate's generic (<T = Document>) is too restrictive, so we change it here to suit our needs.
+    export interface Collection {
+        aggregate<T = any>(pipeline?: MongoDb.Document[], options?: AggregateOptions): AggregationCursor<T>;
     }
 }

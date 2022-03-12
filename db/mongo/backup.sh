1b4110c8b584de87f3389c780edb7fae9687d5db
Ronald M. Clifford <roncli@roncli.com>
2022-03-11 22:17:41 -0800

    Correct the login for backups.

--- db/mongo/backup.sh
+++ db/mongo/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mongodump --gzip --archive=/data/backup/db-$(date +%Y-%m-%d-%H-%M-%S).gz
+mongodump --gzip --archive=/data/backup/db-$(date +%Y-%m-%d-%H-%M-%S).gz -u web_roncli -p $(cat $WEB_SIXGG_PASSWORD_FILE)
 
 find /data/backup -mtime +7 -delete

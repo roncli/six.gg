3ffd10bf75382f6f205af4ecf3538bc64ed31f72
Ronald M. Clifford <roncli@roncli.com>
2022-03-11 22:28:55 -0800

    Fix copy/paste.

--- db/mongo/backup.sh
+++ db/mongo/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mongodump --gzip --archive=/data/backup/db-$(date +%Y-%m-%d-%H-%M-%S).gz -u web_roncli -p $(cat $WEB_SIXGG_PASSWORD_FILE)
+mongodump --gzip --archive=/data/backup/db-$(date +%Y-%m-%d-%H-%M-%S).gz -u web_sixgg -p $(cat $WEB_SIXGG_PASSWORD_FILE)
 
 find /data/backup -mtime +7 -delete

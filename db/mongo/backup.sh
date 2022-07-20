67a79c6863203be19144dcdda4f3d62eb7e21ae2
Ronald M. Clifford <roncli@roncli.com>
2022-07-19 17:09:02 -0700

    Don't try to delete the backup directory.

--- db/mongo/backup.sh
+++ db/mongo/backup.sh
@@ -2,4 +2,4 @@
 
 mongodump --gzip --archive=/data/backup/db-$(date +%Y-%m-%d-%H-%M-%S).gz -u web_sixgg -p $(cat $WEB_SIXGG_PASSWORD_FILE)
 
-find /data/backup -mtime +7 -delete
+find /data/backup/* -mtime +7 -delete

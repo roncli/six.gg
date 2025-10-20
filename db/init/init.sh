b573fa93ca3085a168cf3ddf02a7fdbf17c355d7
roncli <roncli@roncli.com>
2025-10-19 17:32:33 -0700

    Remove Mongo database container.

--- db/init/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mongosh --eval "const WEB_SIXGG_PASSWORD = '$(cat $WEB_SIXGG_PASSWORD_FILE)';" 127.0.0.1:27017 /var/mongo/init.js
-mongosh 127.0.0.1:27017 /var/mongo/migrations/index.js
-touch /data/db/initialized
--- db/mongo/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mongodump --gzip --archive=/data/backup/db-$(date +%Y-%m-%d-%H-%M-%S).gz -u web_sixgg -p $(cat $WEB_SIXGG_PASSWORD_FILE)
-
-find /data/backup/* -mtime +7 -delete
